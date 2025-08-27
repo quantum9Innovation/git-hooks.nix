57f48ead6f4842a879609b39127a6b5d1cd39b73
Ananth Venkatesh <ananth@functor.systems>
2025-08-26 17:13:32 -0700

    feat: upgrade to fourmolu 18

--- nix/tools.nix
+++ nix/tools.nix
@@ -184,7 +184,7 @@ in
     ;
   inherit (elmPackages) elm-format elm-review elm-test;
   # TODO: these two should be statically compiled
-  inherit (haskellPackages) fourmolu;
+  inherit (haskellPackages) fourmolu_0_18_0_0;
   inherit (luaPackages) luacheck;
   inherit (nodePackages) eslint markdownlint-cli prettier cspell;
   inherit (ocamlPackages) ocp-indent;
